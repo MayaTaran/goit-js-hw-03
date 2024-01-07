13e1cd1146daecbedeed5e0184ddd620a34707d0
MayaTaran <145223827+MayaTaran@users.noreply.github.com>
2024-01-07 13:14:16 +0200

    first version hw

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    let summArray = firstArray.concat(secondArray);
+    let resultArray;
+    if (summArray.length > maxLength) {
+        resultArray = summArray.slice(0, maxLength);
+    }
+    else {
+        resultArray = summArray;
+    }
+    return resultArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
